c3928d1911c0d5631bc28433ddc5a68d966ad25c
PushinAP <dast110995@gmail.com>
2018-04-11 11:31:59 +0300

    Delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,16 @@
 class CommentsController < ApplicationController
   def create
   	@article = Article.find(params[:article_id])
-  	puts params
   	@comment = @article.comments.create(comment_params)
 
   	redirect_to article_path(@article)
+  end
+
+   def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   private
